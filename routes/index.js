e458873d4bd418a198d598a8c601fc185b9eb2f7
Jorge Rodriguez <jorgerod84@gmail.com>
2015-07-13 19:02:28 +0200

    Primera pagina y favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Quiz", text1: 'Bienvenido a QUIZ', text2: "El portal donde podrá crear sus propios juegos!" });
 });
 
 module.exports = router;
