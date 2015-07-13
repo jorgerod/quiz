07732efc58263032acab041d55283a14b5e85d09
Jorge Rodriguez <jorgerod84@gmail.com>
2015-07-13 19:31:57 +0200

    primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Quiz", text1: 'Bienvenido a QUIZ', text2: "El portal donde podrá crear sus propios juegos!" });
 });
+
+router.get('/quizes/answer', quizController.answer);
+router.get('/quizes/question', quizController.question);
 
 module.exports = router;
