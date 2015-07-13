bc2082adc8622f60ddcc17302304d74b491ce0f0
Jorge Rodriguez <jorgerod84@gmail.com>
2015-07-13 21:08:15 +0200

    add author

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Quiz", text1: 'Bienvenido a QUIZ', text2: "El portal donde podrá crear sus propios juegos!" });
+  res.render('index', { title: "Quiz", text2: "El portal donde podrá crear sus propios juegos!" });
 });
 
 router.get('/quizes/answer', quizController.answer);
 router.get('/quizes/question', quizController.question);
+router.get('/author', function (req, res, next) {
+	res.render('author.ejs', {author: "Jorge Rodriguez Martin"});
+});
 
 module.exports = router;
