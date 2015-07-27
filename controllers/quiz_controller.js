d86bed46250e972e8a02a9793df096deadf94c44
Jorge Rodriguez <jorgerod84@gmail.com>
2015-07-27 20:09:24 +0200

    cambio success por then

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,13 +2,13 @@
 var models = require('../models/models.js');
 
 exports.question = function (req, res, next) {
-	models.Quiz.findAll().success(function (quiz) {
+	models.Quiz.findAll().then(function (quiz) {
 		res.render('quizes/question', {question: quiz[0].pregunta});
 	});
 };
 
 exports.answer = function (req, res, next) {
-	models.Quiz.findAll().success(function (quiz) {
+	models.Quiz.findAll().then(function (quiz) {
 		if (req.query.answer === quiz[0].respuesta) {
 			res.render('quizes/answer', {answer: 'Correcto'});
 		} else {
